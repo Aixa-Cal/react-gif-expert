b24808890b80db77a55c48d3180cc311369cfbb4
Aixa-Cal <aixacastilloalvarez@gmail.com>
2023-03-05 17:21:49 -0400

    first commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+export const AddCategory = ({onNewCategory}) => {
+  const [inputValue, setinputValue] = useState('');
+
+  const onInputChange = ({ target }) => {
+    setinputValue(target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim().length <= 1) return;
+
+    setinputValue('');
+    onNewCategory(inputValue.trim());
+  
+  };
+
+  return (
+    <form onSubmit={ onSubmit}>
+      <input
+        type="text"
+        placeholder="Buscar GIFs"
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
